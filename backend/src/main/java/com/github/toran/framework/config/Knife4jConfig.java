00dd67f2c362529f3b18fa8afa0e8f79bc1b9767
Toran <www2026263245@163.com>
2025-12-09 13:20:19 +0800

    front test

--- backend/src/main/java/com/github/toran/framework/config/Knife4jConfig.java
+++ backend/src/main/java/com/github/toran/framework/config/Knife4jConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,19 +16,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Knife4jConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Personal Blog API 文档")
-                        .description("个人博客系统 RESTful API 接口文档")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("toran")
-                                .email("toran@example.com")
-                                .url("https://github.com/toran"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Personal Blog API 文档")
+                                                .description("个人博客系统 RESTful API 接口文档")
+                                                .version("1.0.0")
+                                                .contact(new Contact()
+                                                                .name("toran")
+                                                                .email("toran@example.com")
+                                                                .url("https://github.com/toran"))
+                                                .license(new License()
+                                                                .name("Apache 2.0")
+                                                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+        }
+
+        /**
+         * 配置 API 分组，确保接口能被正确扫描和显示
+         *
+         * @return GroupedOpenApi Bean
+         */
+        @Bean
+        public GroupedOpenApi personalBlogApi() {
+                return GroupedOpenApi.builder()
+                                .group("personal-blog")
+                                .packagesToScan("com.github.toran.module")
+                                .pathsToMatch("/api/**")
+                                .build();
+        }
 }
